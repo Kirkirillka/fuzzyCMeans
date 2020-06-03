7b570e6931a3a94a8bd13f46abbd594be39fe8a7
Kir <kirillkukarcev@gmail.com>
2020-06-04 00:15:46 +0300

    Use a squared matrix of points to test clusterization

--- src/main/scala/org/apache/spark/DFuzzyStreamRunner.scala
+++ src/main/scala/org/apache/spark/DFuzzyStreamRunner.scala
@@ -18,24 +18,20 @@ object DFuzzyStreamRunner {
       Vectors.dense(0.0, 0.1),
       Vectors.dense(0.1, 0.0),
       Vectors.dense(9.0, 0.0),
-      Vectors.dense(9.0, 0.2),
-      Vectors.dense(9.2, 0.0),
-      Vectors.dense(5.0, 9.0),
-      Vectors.dense(5.0, 0.1),
-      Vectors.dense(5.1, 3.0),
-      Vectors.dense(9.0, 0.0),
-      Vectors.dense(9.0, 5.2),
-      Vectors.dense(9.2, 0.0),
-      Vectors.dense(1,2),
-        Vectors.dense(1,3)
+      Vectors.dense(9.0, 0.1),
+      Vectors.dense(9.1, 0.0),
+      Vectors.dense(0.0,9.0),
+      Vectors.dense(0.1,9.0),
+      Vectors.dense(0.0,9.1),
+      Vectors.dense(9.0,9.0),
+      Vectors.dense(9.0,9.1),
+      Vectors.dense(9.1,9.0)
     )
     val rdd = sc.parallelize(points, 3).cache()
 
-    val rdd2 = sc.parallelize(points, 3).cache()
+    val model = DFuzzyStream.train(rdd, 2.0,4,4)
 
-    val model = DFuzzyStream.train(rdd, 2.0)
-
-    val fuzzyPredicts = model.fuzzyPredict(rdd2).collect()
+    val fuzzyPredicts = model.fuzzyPredict(rdd).collect()
 
     assert(points.length == fuzzyPredicts.length)
 
